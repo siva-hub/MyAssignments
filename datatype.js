6f9d03b7f2d298aff83d68df34e33db6da41c558
siva-hub <54105248+siva-hub@users.noreply.github.com>
2026-09-02 11:04:07 +0530

    Add files via upload
    Day1

---
+++ datatype.js
@@ -0,0 +1,21 @@
+const { log } = require("node:console")
+
+var firstName = "sivaprakash"
+console.log(firstName)
+console.log(typeof firstName)
+
+var companyName = "iOPEX"
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber = 8056431217
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+var hasPlaywright 
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
