086a4b76d8112bb7a4f93c3277a0f0a7f5339224
siva-hub <54105248+siva-hub@users.noreply.github.com>
2026-09-03 11:03:19 +0530

    Add files via upload

---
+++ numbertype.js
@@ -0,0 +1,14 @@
+var value = -4
+function greaterorlesser (parameter){
+if (parameter>0){
+    console.log("The number is positive")
+}
+else if (parameter<0){
+    console.log("The number is negative")
+}
+else 
+{
+    console.log("The number is equal to zero")
+}
+}
+greaterorlesser(value)
