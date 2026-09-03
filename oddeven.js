873aa453edeb912afebfd37f9cf4517ad1fc19c0
siva-hub <54105248+siva-hub@users.noreply.github.com>
2026-09-03 10:54:01 +0530

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,10 @@
+
+var number = 10
+function isOddOrEven(value) {
+    if(value %2 ==0){
+        console.log("Print Even");
+    } else{
+        console.log("Print odd");
+}
+}
+isOddOrEven(number)
