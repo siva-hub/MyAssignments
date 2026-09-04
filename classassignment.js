56ba5a0289ccf990a31ea8b18632843ef8b646ba
siva-hub <54105248+siva-hub@users.noreply.github.com>
2026-09-04 16:17:33 +0530

    Add files via upload

---
+++ classassignment.js
@@ -0,0 +1,20 @@
+let s = "hello World"
+console.log(s.split(" ")[1].length);
+console.log(s.charAt(10));
+
+let y =  " fly me to the moon ";
+console.log(y.trim());
+console.log(y.trim().split(" ")[4].length);
+
+
+
+let word1 = "listen"
+let word2 = "silent"
+console.log(word1.split("").sort());  //used sort to arrange in alphabetical orders 
+console.log(word2.split("").sort());
+if (word1 ===word1){
+    console.log("True")
+}else
+{
+    console.log("False")
+}
