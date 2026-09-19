3f60c4846f78dcc9e29548ac74da587ba3409e10
siva-hub <54105248+siva-hub@users.noreply.github.com>
2026-09-19 14:43:31 +0530

    Delete Week3/Day 3  directory

--- Week3/Day 3 /classroom.ts
+++
@@ -1,8 +0,0 @@
-type Paymentmethod = "UPI" | "Credit Card" | "Pay pal"
-
-function payment(payementcheck: Paymentmethod) {
-    console.log('method is chosen as', payementcheck)
-}
-
-payment('UPI')
-payment('Credit Card')
